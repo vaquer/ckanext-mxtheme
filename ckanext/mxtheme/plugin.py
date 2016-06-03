9e4f9c3274593b7aea752811613bd4297fa13f19
Frank <akura11.tt@gmail.com>
2016-06-03 14:23:19 -0500

    Resolviendo detalles visuales

--- ckanext/mxtheme/plugin.py
+++ ckanext/mxtheme/plugin.py
@@ -1,13 +1,22 @@
+import datetime
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
-class MxthemePlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
+def format_display_date(time_stamp, format_date="%Y-%m-%d"):
+    date_object = datetime.datetime.strptime(time_stamp, "%Y-%m-%d %H:%M:%S.%f")
+    return date_object.strftime(format_date)
 
+
+class MxthemePlugin(plugins.SingletonPlugin):
     # IConfigurer
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'mxtheme')
+
+    def get_helpers(self):
+        return {'format_display_date': format_display_date}
